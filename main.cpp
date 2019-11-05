bdb8f0b9bac69206792741121850ef833c5bd83a
sergio <srgmatveev@gmail.com>
2019-11-05 19:32:16 +0300

    second commit

--- main.cpp
+++ main.cpp
@@ -6,19 +6,22 @@
 #include <sys/epoll.h>
 #include <zconf.h>
 #include <cstring>
+#include <pthread.h>
 #include "get_opt.h"
 
-#define MAX_EVENTS 32
-struct sockaddr_in sockAddr;
-char *serv_dir = nullptr;
-int server_fd;
+#define MAX_EVENT_NUMBER 1024
+#define BUFFER_SIZE 10
+struct fds {
+    int epollfd;
+    int sockfd;
+};
 
 int set_nonblock(int fd) {
     int flags;
 #if defined(O_NONBLOCK)
     if (-1 == (flags = fcntl(fd, F_GETFL, 0)))
         flags = 0;
-    return fcntl(fd, F_SETFL, (unsigned)flags | O_NONBLOCK);
+    return fcntl(fd, F_SETFL, (unsigned) flags | O_NONBLOCK);
 #else
     flags = 1;
     return ioctl(fd, FIONBIO, &flags);
@@ -26,57 +29,120 @@ int set_nonblock(int fd) {
 
 }
 
+void AddFd(int epollfd, int fd, bool oneshot) {
+    struct epoll_event event;
+    event.data.fd = fd;
+    event.events = EPOLLIN | EPOLLET;
+    if (oneshot)
+        event.events |= EPOLLONESHOT;
+
+    epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &event);
+    set_nonblock(fd);
+}
+
+void reset_oneshot(int &epfd, int &fd) {
+    struct epoll_event event;
+    event.data.fd = fd;
+    event.events = EPOLLIN | EPOLLET | EPOLLONESHOT;
+    epoll_ctl(epfd, EPOLL_CTL_MOD, fd, &event);
+}
+
+void *worker(void *arg) {
+    int sockfd = ((struct fds *) arg)->sockfd;
+    int epollfd = ((struct fds *) arg)->epollfd;
+    printf("start new thread to receive data on fd: %d\n", sockfd);
+    char buf[BUFFER_SIZE];
+    memset(buf, 0, BUFFER_SIZE);
+
+    while (1) {
+        int ret = recv(sockfd, buf, BUFFER_SIZE - 1, 0);
+        if (ret == 0) {
+            close(sockfd);
+            printf("foreigner closed the connection\n");
+            break;
+        } else if (ret < 0) {
+            if (errno = EAGAIN) {
+                reset_oneshot(epollfd, sockfd);
+                printf("read later\n");
+                break;
+            }
+        } else {
+            printf("get content: %s\n", buf);
+            //Hibernate for 5 seconds to simulate data processing
+            printf("worker working...\n");
+            sleep(5);
+        }
+    }
+    printf("end thread receiving data on fd: %d\n", sockfd);
+    pthread_exit(0);
+}
+
+
 int main(const int argc, const char **argv) {
     int masterSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    sockAddr.sin_family = AF_INET;
-    get_command_line(argc, (char **) (argv), sockAddr, serv_dir);
+    if (masterSocket < 0)
+        perror("fail to create socket!\n"), exit(errno);
+    char *serv_dir = nullptr;
+    struct sockaddr_in sock_addr;
+    bzero(&sock_addr, sizeof(sock_addr));
+    sock_addr.sin_family = AF_INET;
+    get_command_line(argc, (char **) (argv), sock_addr, serv_dir);
     int opt = 1;
     if (setsockopt(masterSocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1) {
         perror("setsockopt");
         exit(errno);
     }
-    if (bind(masterSocket, (struct sockaddr *) &sockAddr, sizeof(sockAddr)) < 0) {
-        perror("Bind");
+    if (bind(masterSocket, (struct sockaddr *) &sock_addr, sizeof(sock_addr)) < 0) {
+        perror("fail to bind socket");
         exit(errno);
     }
 
     set_nonblock(masterSocket);
     listen(masterSocket, SOMAXCONN);
-    int efd = epoll_create1(0);
 
-    struct epoll_event event{0};
-    event.data.fd = masterSocket;
-    event.events = EPOLLIN;
-    epoll_ctl(efd, EPOLL_CTL_ADD, masterSocket, &event);
+    struct epoll_event events[MAX_EVENT_NUMBER];
+    int epollfd = epoll_create1(0);
+    if (epollfd == -1)
+        perror("fail to create epoll\n"), exit(errno);
 
-    for (;;) {
-        struct epoll_event events[MAX_EVENTS];
-        int N = epoll_wait(efd, events, MAX_EVENTS, -1);
-        for (size_t i = 0; i < N; ++i) {
-            if (events[i].data.fd == masterSocket) {
-                int slaveSocket = accept(masterSocket, nullptr, nullptr);
-                set_nonblock(slaveSocket);
-                struct epoll_event event_slave{0};
-                event_slave.data.fd = slaveSocket;
-                event_slave.events = EPOLLIN;
-                epoll_ctl(efd, EPOLL_CTL_ADD, slaveSocket, &event_slave);
-            } else {
-                static char BUFFER[1024];
-                memset(BUFFER,'\0',1024);
-                const char *ex = "exit\r\n";
-                int recvResult = recv(events[i].data.fd,
-                                      BUFFER, 1024, MSG_NOSIGNAL);
-                if (!recvResult && errno != EAGAIN) {
-                    shutdown(events[i].data.fd, SHUT_RDWR);
-                    close(events[i].data.fd);
-                } else if (recvResult > 0) {
-                    send(events[i].data.fd, BUFFER, recvResult, MSG_NOSIGNAL);
-                    if (strcmp(ex, BUFFER) == 0)
-                        return 0;
+    AddFd(epollfd, masterSocket, false);
 
-                }
+    for(;;){
+        int ret = epoll_wait(epollfd, events, MAX_EVENT_NUMBER, -1);  //Permanent Wait
+        if(ret < 0)
+        {
+            printf("epoll wait failure!\n");
+            break;
+        }
+        int i;
+        for(i = 0; i < ret; i++)
+        {
+            int sockfd = events[i].data.fd;
+            if(sockfd == masterSocket)
+            {
+                struct sockaddr_in slave_address;
+                socklen_t slave_addrlength = sizeof(slave_address);
+                int slaveSocket = accept(masterSocket, (struct sockaddr*)&slave_address, &slave_addrlength);
+                AddFd(epollfd, slaveSocket, true);
+            }
+            else if(events[i].events & EPOLLIN)
+            {
+                pthread_t thread;
+                struct fds fds_for_new_worker;
+                fds_for_new_worker.epollfd = epollfd;
+                fds_for_new_worker.sockfd = events[i].data.fd;
+                /*Start a new worker thread to serve sockfd*/
+                pthread_create(&thread, NULL, worker, &fds_for_new_worker);
 
             }
+            else
+            {
+                printf("something unexpected happened!\n");
+            }
         }
+
     }
+    close(masterSocket);
+    close(epollfd);
+    return 0;
 }
