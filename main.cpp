d873cb2493df5c1796a51c61d651f4b8f08e68a1
Serge Feiler <srgmatveev@gmail.com>
2019-11-09 19:15:15 +0300

    change back on std::string 1

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@
 #include <regex>
 #include <iostream>
 #include <fstream>
+#include <signal.h>
+#include <sys/stat.h>
 #include "get_opt.h"
 
 std::string serv_dir{};
@@ -130,6 +132,7 @@ inline void f(int &fd, const std::string &request) {
     }
 }
 
+
 void *worker(void *arg) {
     int sockfd = ((struct fds *) arg)->sockfd;
     int epollfd = ((struct fds *) arg)->epollfd;
@@ -161,8 +164,7 @@ void *worker(void *arg) {
     pthread_exit(0);
 }
 
-
-int main(const int argc, const char **argv) {
+int run(const int argc, const char **argv){
     int masterSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (masterSocket < 0)
         perror("fail to create socket!\n"), exit(errno);
@@ -222,4 +224,65 @@ int main(const int argc, const char **argv) {
     close(masterSocket);
     close(epfd);
     return 0;
+}
+
+static void skeleton_daemon()
+{
+    pid_t pid;
+
+    /* Fork off the parent process */
+    pid = fork();
+
+    /* An error occurred */
+    if (pid < 0)
+        exit(EXIT_FAILURE);
+
+    /* Success: Let the parent terminate */
+    if (pid > 0)
+        exit(EXIT_SUCCESS);
+
+    /* On success: The child process becomes session leader */
+    if (setsid() < 0)
+        exit(EXIT_FAILURE);
+
+    /* Catch, ignore and handle signals */
+    //TODO: Implement a working signal handler */
+    signal(SIGCHLD, SIG_IGN);
+    signal(SIGHUP, SIG_IGN);
+
+    /* Fork off for the second time*/
+    pid = fork();
+
+    /* An error occurred */
+    if (pid < 0)
+        exit(EXIT_FAILURE);
+
+    /* Success: Let the parent terminate */
+    if (pid > 0)
+        exit(EXIT_SUCCESS);
+
+    /* Set new file permissions */
+    umask(0);
+
+    /* Change the working directory to the root directory */
+    /* or another appropriated directory */
+    chdir("/");
+
+    /* Close all open file descriptors */
+    int x;
+    for (x = sysconf(_SC_OPEN_MAX); x>=0; x--)
+    {
+        close (x);
+    }
+
+    /* Open the log file */
+   // openlog ("firstdaemon", LOG_PID, LOG_DAEMON);
+}
+
+int main(const int argc, const char **argv) {
+    skeleton_daemon();
+    while (1) {
+        run(argc, argv);
+    }
+    return EXIT_SUCCESS;
 }
